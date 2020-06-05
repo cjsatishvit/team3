c71cc3255d787376123edf072021d2c1a4457f9d
Said33pika <44300933+Said33pika@users.noreply.github.com>
2020-06-06 00:52:58 +0530

    All files added

---
+++ shristi/src/repossession_notices/User_Bean.java
@@ -0,0 +1,33 @@
+package repossession_notices;
+
+public class User_Bean {
+	private String FirstName;
+	private String LastName;
+	private String LoanId;
+	private String DefaulterId;
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String FirstName) {
+		this.FirstName = FirstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String LastName) {
+		this.LastName = LastName;
+	}
+	public String getLoanId() {
+		return LoanId;
+	}
+	public void setLoanId(String LoanId) {
+		this.LoanId = LoanId;
+	}
+	public String getDefaulterId() {
+		return DefaulterId;
+	}
+	public void setDefaulterId(String DefaulterId) {
+		this.DefaulterId = DefaulterId;
+	}
+
+}
---
+++ shristi/src/repossessions/Insert_NoticeValues.java
@@ -0,0 +1,29 @@
+package repossessions;
+import java.sql.PreparedStatement;
+
+import repossession_notices.DB_Connection2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class Insert_NoticeValues {
+	public void insert_noticevalues(String FirstName,String LastName,String LoanId, String  DefaulterId)
+	{
+		 DB_Connection2 obj_DB_Connection2=new DB_Connection2();
+		 Connection connection=obj_DB_Connection2.get_connection();
+		 PreparedStatement ps=null;
+		try {
+	String query="insert into defaulters.foreclosuresnotice(FirstName,LastName,LoanId,DefaulterId) values(?,?,?,?)";
+		 ps=connection.prepareStatement(query);
+		 ps.setString(1, FirstName);
+		 ps.setString(2, LastName);
+		 ps.setString(3, LoanId);
+		 ps.setString(4, DefaulterId);
+		
+		 ps.executeUpdate();
+		} catch (Exception e) {
+		 System.err.println(e);
+		}
+	}
+
+}
